a6b97efac799a6904c4fa181def8af0048c2906d
H@r! <s.hariharancs@gmail.com>
2018-01-25 15:05:56 +0530

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,10 @@
+def main():
+    a = (int(input("Enter a number to find odd or even:")))
+    if (a % 2 == 0):
+        print("Given number is even")
+    else:
+        print("Given number is odd")
+
+
+if __name__ == '__main__':
+    main()
