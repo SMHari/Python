ac59c44ed86e3a2acaacc40cc8ef51bc5d05fabc
H@r! <s.hariharancs@gmail.com>
2018-01-25 15:02:46 +0530

    Create arith.py

---
+++ arith.py
@@ -0,0 +1,19 @@
+def main():
+    a=10
+    b=20
+    c=30
+    d=40
+    e=50
+    f=60
+    g=70
+    h=80
+    i=989
+    j=100
+    print('a+b={0}'.format(a+b))
+    print('c-d={0}'.format(c - d))
+    print('e*f={0}'.format(e * f))
+    print('g/h={0}'.format(g / h))
+    print('i%j={0}'.format(i % j))
+
+if __name__=='__main__':
+    main()
